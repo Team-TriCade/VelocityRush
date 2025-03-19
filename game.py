da5015e8538a7308095cf3c9c00056f5e063a71d
Meow Dev 7 <165454821+meowdev7@users.noreply.github.com>
2025-03-19 20:43:13 +0530

    infinite scrolling bg

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
 import pygame
 from pygame.locals import *
+import math
+
 # initialise pygame
 pygame.init()
+
+# initialise clock
+clock = pygame.time.Clock()
+FPS = 69
+
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -9,14 +16,41 @@ WINDOW_HEIGHT = 720
 screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('Velocity Rush')
 
+# load bg image
+
+bg = pygame.image.load('assets/bg.png').convert()
+bg_width = bg.get_width()
+
+#bg scrolling vars
+bg_pos_x = 0
+scroll_speed = 2 #adjustable
+tiles = math.ceil( WINDOW_WIDTH / bg_width) + 1
+
+
 running = True
 
 while running:
+
+    clock.tick(FPS)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill((0,0,0)) #the background color
+    #move the bg 
+    bg_pos_x -= scroll_speed
+    
+    #check if it is out of screen, if yes then reset its position
+    if bg_pos_x <= -bg_width:
+         bg_pos_x = 0
+         
+    # # drawing the bg
+    # for i in range (0, tiles):
+    #     screen.blit(bg,(i * bg_width , 0))
+        
+    screen.blit(bg, (bg_pos_x,0))
+    screen.blit(bg,(bg_pos_x+bg_width,0)) #second copy to fill the gap. we add the width to ensure that there is no gap
+        
     pygame.display.update()
     
     
