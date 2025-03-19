81e433cde4b585c237fa0ed00dcba827150d45d6
Meow Dev 7 <165454821+meowdev7@users.noreply.github.com>
2025-03-19 20:25:35 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,23 @@
 import pygame
+from pygame.locals import *
+# initialise pygame
+pygame.init()
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption('Velocity Rush')
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((0,0,0)) #the background color
+    pygame.display.update()
+    
+    
+pygame.quit() 
