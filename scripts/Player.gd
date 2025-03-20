cd17977c5b523c11f1c6cda1c441330668dab731
Meow Dev 7 <165454821+meowdev7@users.noreply.github.com>
2025-03-20 21:48:00 +0530

    fixed player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -250.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = $Image
 
 func _physics_process(delta):
 	# Add the gravity.
