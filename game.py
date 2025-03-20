8e8536d5b4ce5de29c198559751479ae704fbfb8
Meow Dev 7 <165454821+meowdev7@users.noreply.github.com>
2025-03-20 19:39:36 +0530

    Co-authored-by: 47 <user-forty-seven@users.noreply.github.com>

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from pygame.locals import *
-import math
+import math 
 
 # initialise pygame
 pygame.init()
@@ -10,8 +10,8 @@ clock = pygame.time.Clock()
 FPS = 69
 
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+WINDOW_WIDTH = 900  # something less to prevent visual glitches while the bg is scrolling
+WINDOW_HEIGHT = 768 # bg's height
 
 screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption('Velocity Rush')
@@ -23,8 +23,13 @@ bg_width = bg.get_width()
 
 #bg scrolling vars
 bg_pos_x = 0
-scroll_speed = 2 #adjustable
+scroll_speed = 3
+to_increment = 0.0069420
 tiles = math.ceil( WINDOW_WIDTH / bg_width) + 1
+
+#colors
+GROUND_COLOR = (139,69,19) #rgb of brown color
+GROUND_HEIGHT = 70 
 
 
 running = True
@@ -32,24 +37,29 @@ running = True
 while running:
 
     clock.tick(FPS)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     #move the bg 
     bg_pos_x -= scroll_speed
+    scroll_speed += to_increment
     
     #check if it is out of screen, if yes then reset its position
     if bg_pos_x <= -bg_width:
-         bg_pos_x = 0
+        bg_pos_x += bg_width  
          
-    # # drawing the bg
-    # for i in range (0, tiles):
-    #     screen.blit(bg,(i * bg_width , 0))
-        
-    screen.blit(bg, (bg_pos_x,0))
-    screen.blit(bg,(bg_pos_x+bg_width,0)) #second copy to fill the gap. we add the width to ensure that there is no gap
+    # drawing the bg
+    for i in range (0, tiles):
+        screen.blit(bg,(i * bg_width , 0))
+
+
+    screen.blit(bg, (bg_pos_x, 0))
+    screen.blit(bg, (bg_pos_x + bg_width, 0))
+    
+    #draw the ground
+    pygame.draw.rect(screen,GROUND_COLOR,(0, WINDOW_HEIGHT-GROUND_HEIGHT,WINDOW_WIDTH+1,GROUND_HEIGHT)) #(x coord, y coord, width, height)
         
     pygame.display.update()
     
